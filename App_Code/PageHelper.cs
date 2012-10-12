09a7412e56a4065d7f800192344d07a451302749
unknown <xzjs@xzjs-PC.(none)>
2012-10-12 12:29:29 +0800

    初始化

---
+++ App_Code/PageHelper.cs
@@ -0,0 +1,130 @@
+﻿using System.Web;
+using System.Web.UI;
+
+/// <summary>
+/// 用于执行页面相关的操作
+/// </summary>
+public static class PageHelper
+{
+    /// <summary>
+    /// 获取当前页面
+    /// </summary>
+    /// <returns>当前页面</returns>
+    public static Page GetCurrentPage()
+    {
+        return (HttpContext.Current.CurrentHandler as Page ?? new Page());
+    }
+    
+    /// <summary>
+    /// 获取添加或修改 Query 参数后的Url
+    /// </summary>
+    /// <param name="name">Query 参数名</param>
+    /// <param name="value">Query 参数值</param>
+    /// <returns></returns>
+    public static string GetUrlWithQuery(string name, string value)
+    {
+        string query = HttpContext.Current.Request.Url.Query;
+        if (name != string.Empty)
+        {
+            if ((query != string.Empty) && (query != "?"))
+            {   //原query不为空
+                int startOfName = query.IndexOf("&" + name + "=");
+                if (startOfName == -1)
+                {
+                    startOfName = query.IndexOf("?" + name + "=");
+                }
+                if (startOfName != -1)
+                {   //含有同名参数
+                    int endOfValue = query.IndexOf("&", startOfName + 1);
+                    if (endOfValue != -1)
+                    {   //同名参数后有其他参数
+                        query = query.Substring(0, startOfName + 1) + name + "=" + value +
+                            query.Substring(endOfValue);
+                    }
+                    else
+                    {   //同名参数后没有其他参数
+                        query = query.Substring(0, startOfName + 1) + name + "=" + value;
+                    }
+                }
+                else
+                {   //不含有同名参数
+                    query += "&" + name + "=" + value;
+                }
+            }
+            else
+            {   //原query不为空
+                query = "?" + name + "=" + value;
+            }
+        }
+        return HttpContext.Current.Request.Url.LocalPath + query;
+    }
+
+    /// <summary>
+    /// 重定向页面并添加或修改 Query 参数
+    /// </summary>
+    /// <param name="name">Query 参数名</param>
+    /// <param name="value">Query 参数值</param>
+    public static void RedirectWithQuery(string name, string value)
+    {
+        Redirect(GetUrlWithQuery(name, value));
+    }
+
+    /// <summary>
+    /// 重定向，其实就是 Response.Redirect()
+    /// </summary>
+    /// <param name="url">重定向到的Url</param>
+    public static void Redirect(string url)
+    {
+        if (GetCurrentPage().ResolveUrl(url) != HttpContext.Current.Request.Url.PathAndQuery)
+        {
+            HttpContext.Current.Response.Redirect(GetCurrentPage().ResolveUrl(url), true);
+        }
+    }
+
+    /// <summary>
+    /// 跳转到结果页面并显示操作结果
+    /// </summary>
+    /// <param name="result">操作结果的描述</param>
+    /// <param name="redirect">显示结果后的自动重定向到的页面</param>
+    public static void ShowResultPage(string result, string redirect = null)
+    {
+        if (redirect != null)
+        {
+            redirect = redirect.ResolveUrl();
+            result = result.PrepareForHtml();
+            result += string.Format("<br /><br /><a href=\"{0}\">如果您的浏览器没有自动跳转，请点击这里</a>", 
+                redirect);
+            HttpContext.Current.Session["ResultRedirect"] = redirect;
+        }
+        HttpContext.Current.Session["Result"] = result;
+        if (HttpContext.Current.Request.Url.AbsolutePath.Contains("/Manage/"))
+        {
+            Redirect("~/Manage/Result.aspx");
+        }
+        else
+        {
+            Redirect("~/Error");
+        }
+    }
+
+    /// <summary>
+    /// 刷新页面
+    /// </summary>
+    public static void Refresh()
+    {
+        HttpContext.Current.Response.Redirect(HttpContext.Current.Request.Url.PathAndQuery);
+    }
+
+    /// <summary>
+    /// 显示带有一段消息和一个确认按钮的警告框
+    /// </summary>
+    /// <param name="page">当前页面</param>
+    /// <param name="message">弹出的对话框中显示的纯文本</param>
+    public static void ShowAlert(string message)
+    {
+        Page page = GetCurrentPage();
+        message = message.Replace('\'', '_');
+        page.ClientScript.RegisterStartupScript(page.GetType(), "message",
+            "<script>alert('"+ message +"');</script>");
+    }
+}
---
+++ App_Code/SqlHelper.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+/// <summary>
+/// 用于执行的 SQL 语句并返回结果
+/// </summary>
+public static class SqlHelper
+{
+    /// <summary>
+    /// 数据库的连接
+    /// </summary>
+    private static SqlConnection conn;
+
+    /// <summary>
+    /// 表示能否正常连接数据库
+    /// </summary>
+    public static bool Connectable
+    {
+        set;
+        get;
+    }
+
+    /// <summary>
+    /// 自此类初始化以来所发生的异常的信息记录
+    /// </summary>
+    public static string ExceptionMessage
+    {
+        set;
+        get;
+    }
+
+    /// <summary>
+    /// 发生异常，添加异常信息记录
+    /// </summary>
+    /// <param name="exception">发生的异常</param>
+    /// <param name="sqlExp">发生异常时的 SQL 语句</param>
+    [System.Diagnostics.Conditional("DEBUG")]
+    private static void ExceptionOccur(Exception exception, string sqlExp = "")
+    {
+        ExceptionMessage += exception.ToString() + "\n";
+        if (sqlExp != "")
+        {
+            ExceptionMessage += "\n执行的 SQL 语句：\n" + sqlExp + "\n";
+        }
+    }
+
+    /// <summary>
+    /// 初始化数据库的连接
+    /// </summary>
+    static SqlHelper()
+    {
+        try
+        {
+            ExceptionMessage = "";
+            string connStr = ConfigurationManager.ConnectionStrings["Tibet"].ConnectionString;
+            conn = new SqlConnection(connStr);
+            conn.Open();
+            Connectable = true;
+        }
+        catch (Exception ex)
+        {
+            conn = new SqlConnection();
+            Connectable = false;
+            ExceptionOccur(ex);
+        }
+        finally
+        {
+            conn.Close();
+        }
+    }
+
+    /// <summary>
+    /// 执行查询，并返回查询所返回的结果集中第一行的第一列，忽略其他列或行
+    /// </summary>
+    /// <param name="sqlExp">查询的 SQL 语句</param>
+    /// <returns>结果集中第一行的第一列，如果结果集为空则为空引用</returns>
+    public static object ExecuteScalar(string sqlExp)
+    {
+        object result = null;
+        try
+        {
+            using (SqlCommand cmd = new SqlCommand(sqlExp, conn))
+            {
+                conn.Open();
+                result = cmd.ExecuteScalar();
+            }
+        }
+        catch (Exception ex)
+        {
+            ExceptionOccur(ex, sqlExp);
+        }
+        finally
+        {
+            conn.Close();
+        }
+        return result;
+    }
+
+    /// <summary>
+    /// 执行查询，并返回查询所返回的结果集中第一个表，忽略其他表
+    /// </summary>
+    /// <param name="sqlExp">查询的 SQL 语句</param>
+    /// <returns>结果集中第一个表，如果结果集为空则为空表</returns>
+    public static DataTable ExecuteAdapter(string sqlExp)
+    {
+        DataTable dt = new DataTable();
+        try
+        {
+            using (SqlDataAdapter da = new SqlDataAdapter(sqlExp, conn))
+            {
+                da.Fill(dt);
+            }
+        }
+        catch (Exception ex)
+        {
+            ExceptionOccur(ex, sqlExp);
+        }
+        finally
+        {
+            conn.Close();
+        }
+        return dt;
+    }
+
+    /// <summary>
+    /// 执行 SQL 语句，并返回受影响的行数
+    /// </summary>
+    /// <param name="sqlExp">执行的 SQL 语句</param>
+    /// <returns>受影响的行数，发生异常时返回 -1 </returns>
+    public static int ExecuteNonQuery(string sqlExp)
+    {
+        int result = -1;
+        try
+        {
+            using (SqlCommand cmd = new SqlCommand(sqlExp, conn))
+            {
+                conn.Open();
+                result = cmd.ExecuteNonQuery();
+            }
+        }
+        catch (Exception ex)
+        {
+            ExceptionOccur(ex, sqlExp);
+        }
+        finally
+        {
+            conn.Close();
+        }
+        return result;
+    }
+}
